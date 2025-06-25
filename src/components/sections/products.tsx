8180268b06d9aa2bc69ecae8113e6d7959c6f817
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-06-25 17:15:48 +0530

    add dots bg to products section

--- src/components/sections/products.tsx
+++ src/components/sections/products.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export default function Products() {
@@ -31,7 +32,15 @@ export default function Products() {
     ];
 
     return (
-        <section className="max-w-7xl px-8 py-16">
+        <section className="max-w-7xl px-8 py-16 relative">
+            <div
+                className={cn(
+                    "absolute inset-0",
+                    "[background-size:20px_20px]",
+                    "[background-image:radial-gradient(rgba(0,0,0,0.5)_1px,transparent_1px)]",
+                    "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]"
+                )}
+            />
             <div className="grid gap-6">
                 {/* <span className="font-display font-bold text-3xl text-center">
                     Products
@@ -39,7 +48,10 @@ export default function Products() {
 
                 <div className="grid gap-6">
                     {products.map((item, index) => (
-                        <div className="grid drop-shadow-2xl border border-black/50" key={index}>
+                        <div
+                            className="grid drop-shadow-2xl border border-black/50"
+                            key={index}
+                        >
                             <Image
                                 width={2560}
                                 height={1249}
